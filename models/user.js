50f6c28db020140a4608cff7f5090d865cc7e4dc
Ayodeji Onawunmi <dejionawunmi@trentu.ca>
2024-07-28 10:05:50 -0400

    10th commit

--- models/user.js
+++ models/user.js
@@ -1,5 +1,11 @@
 const mongoose = require("mongoose");
 
+// Task schema and model
+const TaskSchema = new mongoose.Schema({
+  text: { type: String, required: true },
+});
+
+
 //User Schema for the MongoDB
 const UserSchema = new mongoose.Schema({
   name: 
@@ -18,6 +24,8 @@ const UserSchema = new mongoose.Schema({
     type: String,
     required: true, 
   },
+  tasks:[TaskSchema],
+  
 },{timestamps: true,});
 
 module.exports = mongoose.model('User', UserSchema);
